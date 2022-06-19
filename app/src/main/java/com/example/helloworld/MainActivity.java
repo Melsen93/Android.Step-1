c57c0fee995a9a400b2a9c2cb8861c370d4971ac
Melsen93 <melsen@yandex.ru>
2022-06-19 19:19:27 +0300

    Переработка
    Добавление редактируемого поля

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -5,10 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private Button button;
-
+    private EditText editText;
+    private TextView textResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,12 +21,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         button = (Button) findViewById(R.id.button1);
+        editText = (EditText) findViewById(R.id.editTextOne);
+        textResult = (TextView) findViewById(R.id.resultText);
+
         button.setText("Ollolo");
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                button.setText("Текст поменялся");
-            }
-        });
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        String srcText = editText.getText().toString();
+        String result = srcText.toUpperCase();
+        textResult.setText(result);
     }
 }
